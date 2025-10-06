846610ea6f6d648ed7873f721503a8cb2b8cd973
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-10-06 21:24:50 +0200

    Inyeccion de dependencias en Arquitectura

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+using Dominio.Ports.Primary;
+using Dominio.Ports.Secundary;
+using Dominio.Services;
+using JsonRepository;
+using Microsoft.Extensions.DependencyInjection;
+
+string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"product.json");
+
+
+
+var services = new ServiceCollection();
+services.AddTransient<IRepository>(provider => new ProductRepository(path));
+
+services.AddTransient<IService,ProductService>();
+
+var serviceProvider =services.BuildServiceProvider();
+var productService = serviceProvider.GetRequiredService<IService>();
+
+
